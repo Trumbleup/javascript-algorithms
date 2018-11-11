d3c5dafbce705a12e427419f4e6ca9343f108875
Danny Trumble <dtrumble12@gmail.com>
2018-11-11 14:45:22 -0600

    Added an algorithm

--- challenges.js
+++ challenges.js
@@ -211,3 +211,36 @@ function getIndexToIns(arr, num) {
 
 
 getIndexToIns([40, 60], 50);
+
+
+// Return true if the string in the first element of the array 
+// contains all of the letters of the string in the second element of 
+// the array.
+// For example, ["hello", "Hello"], should return true because 
+// all of the letters in the second string are present in the first, 
+// ignoring case.
+// The arguments ["hello", "hey"] should return false because the 
+// string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because
+//  all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+  let arr0 = arr[0].toLowerCase().split("").join(" ");
+  let arr1 = arr[1].toLowerCase().split("").sort();
+  let answer = false;
+
+  for (let i = 0 ; i < arr1.length ; i++ ) {
+    if (arr0.includes(arr1[i])) {
+      answer = true
+    }
+
+    else {
+      return false
+    }
+  };
+  if (answer === true) {
+    return true
+  } 
+}
+
+mutation(["hello", "Hello"]);
