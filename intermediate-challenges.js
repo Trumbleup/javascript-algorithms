64dddad326c54f8635651593904cb2d9ba23a71e
Danny Trumble <dtrumble12@gmail.com>
2019-02-11 23:26:53 -0600

    Added more intermediate-challenges

--- intermediate-challenges.js
+++ intermediate-challenges.js
@@ -193,3 +193,75 @@ function fearNotLetter(str) {
 
 fearNotLetter("abce");
 fearNotLetter("abcdefghijklmnopqrstuvwxyz");
+
+// Intermediate Algorithm Scripting: Sorted Union
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+function uniteUnique(arr) {
+  const newArrObj = arguments;
+  const answer = [];
+  for (let arrArg in newArrObj) {
+    newArrObj[arrArg].map( i =>{
+      if (answer.indexOf(i) === -1) {
+        answer.push(i)
+      }
+    })
+  }
+
+  return answer;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// Intermediate Algorithm Scripting: Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  const htmlEnts = [["&", "&amp;"], ["<", "&lt;"], [">", "&gt;"], ['"', "&quot;"], ["'", "&apos;"]];
+  const strArr = str.split("");
+
+  strArr.map( (val, i) => {
+    htmlEnts.map( htmlArr => {
+      if (val === htmlArr[0]) {
+        strArr.splice(i, 1,  htmlArr[1])
+      }
+    })
+  })
+
+  return strArr.join("");
+}
+
+convertHTML("Dolce & Gabbana");
+
+
+// Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+  const fibArr = [1, 1];
+  let oddSum = 0
+  let sum = 1;
+  while(sum < num) {
+    sum = sum + fibArr[fibArr.length-2];
+    if(sum <= num) {
+      fibArr.push(sum);
+    } 
+  }
+  fibArr.map(val => {
+    if(val % 2 !== 0) {
+      oddSum = oddSum + val
+    }
+  })
+  return oddSum;
+}
+
+sumFibs(4);
